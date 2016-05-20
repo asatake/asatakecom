a801541a3225123416e0a8ef503873d7eb58a4c3
asatake <asataken@gmail.com>
2016-05-20 16:51:54 +0900

    色々テスト

--- asatakecom.py
+++ asatakecom.py
@@ -1,12 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
-
+@app.route('/<name>')
+def index(name=None):
+    return render_template('index.html', name=name, title='Top')
 
 if __name__ == '__main__':
     app.run()

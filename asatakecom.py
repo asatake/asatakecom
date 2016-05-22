5f6206115273408f2c1719345592459b41e1dac5
asatake <asataken@gmail.com>
2016-05-22 09:59:23 +0900

    かなり変更した

--- asatakecom.py
+++ asatakecom.py
@@ -1,6 +1,16 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, redirect, request, url_for
+from flask.ext.assets import Environment, Bundle
+from flaskext.markdown import Markdown
 
 app = Flask(__name__)
+Markdown(app)
+assets = Environment(app)
+assets.url = app.static_url_path
+scss = Bundle(
+    'scss/variables.scss', 'scss/common.scss',
+    filters='pyscss', output='css/common.css'
+)
+assets.register('scss_all', scss)
 
 
 @app.route('/')
@@ -8,5 +18,25 @@ app = Flask(__name__)
 def index(name=None):
     return render_template('index.html', name=name, title='Top')
 
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About')
+
+
+@app.route('/music')
+def music():
+    return render_template('music.html', title='Music')
+
+
+@app.route('/works')
+def works():
+    return render_template('works.html', title='Works')
+
+
+@app.route('/link')
+def link():
+    return render_template('link.html', title='Links')
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
